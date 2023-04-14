88f1fb8432169996222ee76aacf79a034c0c2b1f
samuelcwilliams <samcwilliams67@gmail.com>
2023-04-14 01:05:49 -0500

    got rever_dns_lookup.py working... i think

--- hw4/reverse_dns_lookup.py
+++ hw4/reverse_dns_lookup.py
@@ -5,21 +5,24 @@ with open('domains_to_ips.csv', 'r') as input_file, open('ips_to_domains.csv', '
 
     # iterate over each line
     for line in input_file:
-
-        # split each line using tabs as a separator
-        columns = line.strip().split(',')
+        # split each line using commas as a separator
+        columns = line.split(',')
 
         # split the values in the second column 
-        addresses = columns[0].split()
+        addresses = columns[1].split()
 
-        # use the subprocess module to perform reverse DNS lookup using the hosts module
         domain = ""
         try:
+            # iterate over each ip in our addresses list
             for ip in addresses:
+                # use the subprocess module to perform reverse DNS lookup using the hosts module
                 output = subprocess.check_output(['host','-t', 'PTR', ip], universal_newlines=True)
-                domain += output.split()[-1]
+
+                # string formatting
+                domain += " " + output.split()[-1]
+                domain = domain.rstrip(".")
+
+                # write the output domain/addresses to the output file
+                output_file.write(f"{ip},{domain}\n")
         except:
             pass
-
-        # write the output domain/addresses to the output file
-        output_file.write(f"{ip},{domain}\n")
